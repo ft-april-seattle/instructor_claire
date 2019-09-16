9c1ae1552d645885a47d1b78b1307088ef52f79a
Mary (Claire) Elliott <49691958+marycelliott@users.noreply.github.com>
2019-09-16 18:00:08 -0500

    Update http.service.ts

--- mean/angular/hello-angular/public/src/app/http.service.ts
+++ mean/angular/hello-angular/public/src/app/http.service.ts
@@ -6,11 +6,6 @@ import { Injectable } from '@angular/core';
 })
 export class HttpService {
   constructor(private _http: HttpClient) { 
-    this.getTasks();
-    this.getTask("5d28b45fb2c2a74560e7d8d9");
-    this.createTask({title: "Something cool", description: "something equally cool", isCompleted: false});
-    this.updateTask("5d28b45fb2c2a74560e7d8d9", {title: "Something cool", description: "something equally cool", isCompleted: false});
-    this.deleteTask("5d28b45fb2c2a74560e7d8d9");
   }
   getTasks(){
     console.log("In the Service!");
